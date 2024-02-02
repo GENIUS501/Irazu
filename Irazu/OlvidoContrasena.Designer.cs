a4156b18f72798e10ca1ff6bb08fa01d14c5582f
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2024-02-01 21:00:04 -0600

    Ajustes

---
+++ Irazu/OlvidoContrasena.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace Lienzos
+{
+    partial class OlvidoContrasena
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_correo = new System.Windows.Forms.TextBox();
+            this.btn_generar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 13);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Correo electronico";
+            // 
+            // txt_correo
+            // 
+            this.txt_correo.Location = new System.Drawing.Point(12, 31);
+            this.txt_correo.Name = "txt_correo";
+            this.txt_correo.Size = new System.Drawing.Size(204, 20);
+            this.txt_correo.TabIndex = 28;
+            // 
+            // btn_generar
+            // 
+            this.btn_generar.Location = new System.Drawing.Point(71, 57);
+            this.btn_generar.Name = "btn_generar";
+            this.btn_generar.Size = new System.Drawing.Size(93, 23);
+            this.btn_generar.TabIndex = 30;
+            this.btn_generar.Text = "Generar token";
+            this.btn_generar.UseVisualStyleBackColor = true;
+            this.btn_generar.Click += new System.EventHandler(this.btn_generar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // OlvidoContrasena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 92);
+            this.Controls.Add(this.btn_generar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_correo);
+            this.Name = "OlvidoContrasena";
+            this.Text = "OlvidoContrasena";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_correo;
+        private System.Windows.Forms.Button btn_generar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Irazu/OlvidoContrasena.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lienzos
+{
+    public partial class OlvidoContrasena : Form
+    {
+        public OlvidoContrasena()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_generar_Click(object sender, EventArgs e)
+        {
+            bool ok = false;
+            if (this.txt_correo.Text == "")
+            {
+                errorProvider1.SetError(this.txt_correo, "Debe ingresar el correo");
+                ok = true;
+            }
+            String expresion;
+            expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(this.txt_correo.Text, expresion))
+            {
+                if (Regex.Replace(this.txt_correo.Text, expresion, String.Empty).Length == 0)
+                {
+
+                }
+                else
+                {
+                    errorProvider1.SetError(this.txt_correo, "Formato de correo invalido");
+                    ok = true;
+                }
+            }
+            else
+            {
+                errorProvider1.SetError(this.txt_correo, "Formato de correo invalido");
+                ok = true;
+            }
+            if (!ok)
+            {
+
+            }
+        }
+    }
+}
