ea8a27d1da554641a4c5654cb525d50c3bc8299c
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2024-02-12 15:45:57 -0600

    asd

---
+++ Irazu/ListaPuestos.cs
@@ -0,0 +1,228 @@
+﻿using Entidades;
+using Negocios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lienzos
+{
+    public partial class ListaPuestos : Form
+    {
+        int valorcelda = -1;
+        public int Usuario { get; set; }
+        public int Id_Rol { get; set; }
+        public ListaPuestos()
+        {
+            InitializeComponent();
+        }
+
+        private void ListaPuestos_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                NPuestos Negocios = new NPuestos();
+                dat_principal.DataSource = Negocios.Mostrar();
+                valorcelda = -1;
+                this.dat_principal.ReadOnly = true;
+                NRoles NegociosRoles = new NRoles();
+                List<EPermisos> perm = new List<EPermisos>();
+                perm = NegociosRoles.llenar_Permisos(Id_Rol, "Puestos");
+                if (perm.Where(x => x.Accion == "Agregar").FirstOrDefault() != null)
+                {
+                    this.btn_agregar.Enabled = true;
+                }
+                if (perm.Where(x => x.Accion == "Modificar").FirstOrDefault() != null)
+                {
+                    this.btn_editar.Enabled = true;
+                }
+                if (perm.Where(x => x.Accion == "Eliminar").FirstOrDefault() != null)
+                {
+                    this.btn_eliminar.Enabled = true;
+                }
+                if (perm.Where(x => x.Accion == "Consultar").FirstOrDefault() != null)
+                {
+                    this.btn_consultar.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dat_principal_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (this.dat_principal.Rows[e.RowIndex].Cells[0].Value.ToString() == "")
+                {
+                    ListaPuestos_Load(null, null);
+                }
+                else
+                {
+                    valorcelda = int.Parse(this.dat_principal.Rows[e.RowIndex].Cells[0].Value.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Renderizar(string Accion)
+        {
+            try
+            {
+
+                if (Accion != "A" && Accion == "M" && valorcelda != -1 || Accion != "A" && Accion == "E" && valorcelda != -1 || Accion != "A" && Accion == "C" && valorcelda != -1)
+                {
+                    //valorcelda = dat_usuarios.CurrentRow.Index;
+                    MantenimientoTipoMedicamento frm = new MantenimientoTipoMedicamento();
+                    frm.Accion = Accion;
+                    frm.Id = valorcelda;
+                    frm.Usuario = Usuario;
+                    frm.ShowDialog();
+                    valorcelda = -1;
+                    ListaPuestos_Load(null, null);
+                }
+                else
+                {
+                    if (Accion == "A")
+                    {
+                        MantenimientoTipoMedicamento frm = new MantenimientoTipoMedicamento();
+                        frm.Accion = Accion;
+                        frm.Id = valorcelda;
+                        frm.Usuario = Usuario;
+                        frm.ShowDialog();
+                        valorcelda = -1;
+                        ListaPuestos_Load(null, null);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Debe seleccionar un Puesto!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.txt_buscar_cedula.Text != "")
+                {
+                    NPuestos Negocios = new NPuestos();
+                    this.dat_principal.DataSource = Negocios.Mostrar().Where(x => x.Id_Puesto == int.Parse(this.txt_buscar_cedula.Text)).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_buscar_nombre_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.txt_nombre.Text != "")
+                {
+                    NPuestos Negocios = new NPuestos();
+                    this.dat_principal.DataSource = Negocios.Mostrar().Where(x => x.Nombre == this.txt_nombre.Text).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_agregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Renderizar("A");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_editar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Renderizar("M");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (valorcelda != -1)
+                {
+                    DialogResult dr = MessageBox.Show("Realmente desea eliminar el Puesto?", "Eliminar el Puesto", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (dr == DialogResult.Yes)
+                    {
+                        Int32 FilasAfectadas = 0;
+                        NPuestos Negocios = new NPuestos();
+                        FilasAfectadas = Negocios.Eliminar(valorcelda, Usuario);
+                        if (FilasAfectadas > 0)
+                        {
+                            MessageBox.Show("Puesto eliminado satisfactoriamente", "Excelente", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            if (FilasAfectadas == -1)
+                            {
+                                MessageBox.Show("Puesto eliminado satisfactoriamente", "Excelente", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                MessageBox.Show("Error al registrar la transaccion.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Error al eliminar el Puesto!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                        valorcelda = -1;
+                        ListaPuestos_Load(null, null);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Debe seleccionar un Puesto!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("El registro que esta tratando de eliminar esta relacionado con otro.\nPor favor verifique antes de borrar este registro.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_consultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Renderizar("C");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Irazu/MantenimientoPuestos.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Lienzos
+{
+    partial class MantenimientoPuestos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MantenimientoPuestos));
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.Grp_Tipo_Productos = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.txt_descripcion = new System.Windows.Forms.TextBox();
+            this.txt_id = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Grp_Tipo_Productos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.Location = new System.Drawing.Point(172, 322);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(83, 23);
+            this.btnCancelar.TabIndex = 35;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.btnAceptar.Image = ((System.Drawing.Image)(resources.GetObject("btnAceptar.Image")));
+            this.btnAceptar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAceptar.Location = new System.Drawing.Point(56, 322);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(79, 23);
+            this.btnAceptar.TabIndex = 34;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // Grp_Tipo_Productos
+            // 
+            this.Grp_Tipo_Productos.Controls.Add(this.pictureBox1);
+            this.Grp_Tipo_Productos.Controls.Add(this.lbl_id);
+            this.Grp_Tipo_Productos.Controls.Add(this.txt_descripcion);
+            this.Grp_Tipo_Productos.Controls.Add(this.txt_id);
+            this.Grp_Tipo_Productos.Controls.Add(this.label5);
+            this.Grp_Tipo_Productos.Controls.Add(this.txt_nombre);
+            this.Grp_Tipo_Productos.Controls.Add(this.label2);
+            this.Grp_Tipo_Productos.Location = new System.Drawing.Point(12, 12);
+            this.Grp_Tipo_Productos.Name = "Grp_Tipo_Productos";
+            this.Grp_Tipo_Productos.Size = new System.Drawing.Size(323, 304);
+            this.Grp_Tipo_Productos.TabIndex = 33;
+            this.Grp_Tipo_Productos.TabStop = false;
+            this.Grp_Tipo_Productos.Text = "Datos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Lienzos.Properties.Resources.Imagen_de_WhatsApp_2023_11_22_a_las_19_30_10_be162142;
+            this.pictureBox1.Location = new System.Drawing.Point(195, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 73;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Location = new System.Drawing.Point(6, 47);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(117, 13);
+            this.lbl_id.TabIndex = 18;
+            this.lbl_id.Text = "Id tipo de medicamento";
+            // 
+            // txt_descripcion
+            // 
+            this.txt_descripcion.Location = new System.Drawing.Point(9, 167);
+            this.txt_descripcion.Multiline = true;
+            this.txt_descripcion.Name = "txt_descripcion";
+            this.txt_descripcion.Size = new System.Drawing.Size(291, 92);
+            this.txt_descripcion.TabIndex = 2;
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(9, 63);
+            this.txt_id.Mask = "000000000000";
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(78, 20);
+            this.txt_id.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Descripcion";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(9, 113);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(166, 20);
+            this.txt_nombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Nombre";
+            // 
+            // MantenimientoPuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 349);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.Grp_Tipo_Productos);
+            this.Name = "MantenimientoPuestos";
+            this.Text = "MantenimientoPuestos";
+            this.Grp_Tipo_Productos.ResumeLayout(false);
+            this.Grp_Tipo_Productos.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.GroupBox Grp_Tipo_Productos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.TextBox txt_descripcion;
+        private System.Windows.Forms.MaskedTextBox txt_id;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label2;
+    }
+}
